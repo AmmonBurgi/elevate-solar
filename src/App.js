c1e416950e447324e2a6835e1cfe127fe8bad9ff
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2021-01-11 17:15:07 -0700

    landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import routes from './routes'
+import Header from './Components/Header/Header'
 import './App.css';
 
-function App() {
+function App(){
+  const [fadeToggle, setFadeToggle] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeToggle(true)
+    }, 100);
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className="App">
-      {routes}
+      <Header />
+      <div className={fadeToggle === false ? 'zero' : 'app-component'}>
+        {routes}
+      </div>
     </div>
   );
 }
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -3,7 +3,38 @@ import React from 'react'
 function Landing(){
     return(
         <div>
-            Landing Component
+            <section className='landing-top-section'>
+                <div className='landing-nav-links'>
+
+                </div>
+                <div className='landing-meet'>
+
+                </div>
+            </section>
+            <hr></hr>
+            <section className='landing-bottom-section'>
+                <p></p>
+                <div>
+                    <nav>
+                        <p></p>
+                        <p></p>
+                    </nav>
+                    <img />
+                </div>
+                <div>
+                    <p></p>
+                    <nav>
+                        <p></p>
+                        <img />
+                    </nav>
+                </div>
+                <div>
+                    <p></p>
+                    <img />
+                    <p></p>
+                    <p></p>
+                </div>
+            </section>
         </div>
     )
 }
