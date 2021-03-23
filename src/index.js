d09a154b34d3763445c639d6174ba39483540838
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2021-03-23 14:54:12 -0600

    router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter} from 'react-router-dom'
+import {HashRouter, BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Router = process.env.NODE_ENV === 'development' ? HashRouter: BrowserRouter;
+
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
